1ce1796a82e66461a92c3a50d53200a4dbbe21b6
Patrik Wehrli <weh@semabit.ch>
2014-06-11 19:43:47 +0200

    use async methods to load and safe proposals

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,6 +3,7 @@ package controllers;
 import controllers.routes;
 import models.*;
 import play.data.Form;
+import play.libs.F.*;
 import play.mvc.Controller;
 import play.mvc.Result;
 
@@ -10,24 +11,36 @@ public class Application extends Controller {
 
     private static final Form<Proposal> proposalForm = Form.form(Proposal.class);
     
-    public static Result index() {
-        Proposal keynote = Proposal.findKeynote();
-        return ok(views.html.index.render(keynote));
+    public static Promise<Result> index() {
+        Promise<Proposal> keynote = Proposal.findKeynote();
+        Promise<Result> result = keynote.map(new Function<Proposal, Result>() {
+
+            @Override
+            public Result apply(Proposal keynote) throws Throwable {
+                return ok(views.html.index.render(keynote));
+            }
+        });
+        return result;
     }
 
     public static Result newProposal() {
         return ok(views.html.newProposal.render(proposalForm));
     }
 
-    public static Result submit() {
+    public static Promise<Result> submit() {
         Form<Proposal> submittedForm = proposalForm.bindFromRequest();
         if (submittedForm.hasErrors()) {
-            return ok(views.html.newProposal.render(submittedForm));
+            return Promise.<Result>pure(ok(views.html.newProposal.render(submittedForm)));
         } else {
             Proposal proposal = submittedForm.get();
-            proposal.save();
-            flash("message", "Thanks for submitting a proposal");
-            return redirect(routes.Application.index());
+            Promise<Result> result = proposal.asyncSave().map(new Function<Void, Result>() {
+                @Override
+                public Result apply(Void aVoid) throws Throwable {
+                    flash("message", "Thanks for submitting a proposal");
+                    return redirect(routes.Application.index());
+                }
+            });
+            return result;
         }
     }
 }
--- app/models/Proposal.java
+++ app/models/Proposal.java
@@ -1,10 +1,16 @@
 package models;
 
 import javax.validation.*;
+
+import play.Logger;
 import play.data.validation.Constraints.*;
 import play.db.ebean.Model;
 
 import javax.persistence.*;
+
+import play.libs.Akka;
+import play.libs.F.*;
+import scala.concurrent.ExecutionContext;
 
 /**
  * Created by weh on 6/4/14.
@@ -37,7 +43,40 @@ public class Proposal extends Model{
 
     private static Finder<Long,Proposal> find = new Finder<Long, Proposal>(Long.class, Proposal.class);
 
-    public static Proposal findKeynote() {
-        return find.where().eq("type", SessionType.Keynote).findUnique();
+    private static ExecutionContext ctx = Akka.system().dispatchers().lookup("akka.db-dispatcher");
+
+    public static Promise<Proposal> findKeynote() {
+        return Promise.promise(new Function0<Proposal>() {
+            @Override
+            public Proposal apply() throws Throwable {
+                return find.where().eq("type", SessionType.Keynote).findUnique();
+            }
+        }, ctx).recover(new Function<Throwable, Proposal>() {
+
+            @Override
+            public Proposal apply(Throwable t) throws Throwable {
+                Logger.error("failed to fetch keynot information", t);
+                Proposal p = new Proposal();
+                p.title = "COMING SOON!";
+                p.proposal = "";
+                Speaker speaker = new Speaker();
+                speaker.name = "";
+                speaker.pictureUrl = "";
+                speaker.twitterId = "";
+                p.speaker = speaker;
+                return p;
+            }
+        }, ctx);
+    }
+
+
+    public Promise<Void> asyncSave() {
+        return Promise.promise(new Function0<Void>() {
+            @Override
+            public Void apply() throws Throwable {
+                save();
+                return null;
+            }
+        }, ctx);
     }
 }
